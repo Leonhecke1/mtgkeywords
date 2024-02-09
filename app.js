2c88a0ec20ff1f340e38ba3a4ab0ca5ad2703114
Leon-Luca Hecke <email@leon-luca.de>
2024-02-09 10:50:50 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const imageGrid = document.getElementById('imageGrid');
+    
+   
+    const imageUrls = [
+      'images/Mtga_flying.png',
+      'images/Mtga_first_strike.png',
+      'images/Mtga_double_strike.png',
+      'images/Mtga_deathtouch.png',
+      'images/Mtga_hexproof.png',
+      'images/Mtga_indestructible.png',
+      'images/Mtga_lifelink.png',
+      'images/Menace.png',
+      'images/Duels_Reach_symbol.png',
+      'images/Mtga_trample.png',
+      'images/Mtga_vigilance.png',
+
+      
+    ];
+  
+    imageUrls.forEach(url => {
+        const img = document.createElement('img');
+        img.src = url;
+        img.classList.add('grid-image');
+        img.addEventListener('click', function() {
+          this.classList.toggle('opacity');
+        });
+        imageGrid.appendChild(img);
+      });
+    });
